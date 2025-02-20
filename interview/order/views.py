dff058a9bed130a33e69c964e25b88808158587f
Hasnain Mahmood <hasnain.mahmood1995@gmail.com>
2025-02-19 20:27:03 -0500

    challenge 2

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework import generics
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
 
@@ -13,3 +17,28 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+    
+    
+
+class DeactivateOrderView(APIView):
+    """
+    View to deactivate an order by setting its is_active field to False.
+    """
+    def patch(self, request, *args, **kwargs):
+
+        order_id = kwargs.get('id')
+        order = get_object_or_404(Order, id=order_id)
+        
+        if not order.is_active:
+            return Response(
+                {"detail": "Order is already deactivated."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        
+
+        order.is_active = False
+        order.save()
+        
+
+        serializer = OrderSerializer(order)
+        return Response(serializer.data, status=status.HTTP_200_OK)
